063791cf54c65060396d65e4d7d18c1826769d4c
DanilaMaster2017 <corotckow94@yandex.ru>
2021-04-23 18:57:19 +0300

    #8 rounding a TransparentImgBackground

--- frontend/src/interface/TransparentImgBackground.tsx
+++ frontend/src/interface/TransparentImgBackground.tsx
@@ -5,12 +5,14 @@ import { FC } from 'react';
 import whiteKing from '../images/chessPieces/wk.svg';
 
 export const TransparentImgBackground: FC = ({ children }) => {
+    const borderRadius = '5px';
     return (
         <div
             css={css`
                 margin: 20px 50px;
                 flex: 1 1 auto;
                 min-width: 450px;
+                border-radius: ${borderRadius} 0 0 ${borderRadius};
                 background-color: white;
                 background-image: url(${whiteKing});
                 background-repeat: no-repeat;
@@ -20,6 +22,7 @@ export const TransparentImgBackground: FC = ({ children }) => {
         >
             <div
                 css={css`
+                    border-radius: ${borderRadius} 0 0 ${borderRadius};
                     background-color: rgba(255, 255, 255, 0.5);
                     width: 100%;
                     height: 100%;
