ba32d6fe7ebf7a4c19132a65c084f78f628e059c
DanilaMaster2017 <corotckow94@yandex.ru>
2021-04-23 19:08:25 +0300

    #9 added bouncing to button

--- frontend/src/interface/Button.tsx
+++ frontend/src/interface/Button.tsx
@@ -3,10 +3,16 @@
 import { css, jsx } from '@emotion/react';
 import { FC } from 'react';
 
-export const Button: FC = ({ children }) => {
+interface Props {
+    onClick: () => void;
+}
+
+export const Button: FC<Props> = ({ onClick, children }) => {
     return (
         <button
+            onClick={onClick}
             css={css`
+                position: relative;
                 font-family: 'Roboto', sans-serif;
                 border: 1px solid black;
                 box-shadow: 2px 2px 3px rgba(0, 0, 0, 0.5);
@@ -15,14 +21,16 @@ export const Button: FC = ({ children }) => {
                 border-radius: 10px;
                 background-color: white;
                 color: black;
+                text-transform: lowercase;
                 font-size: 15px;
                 &:hover {
-                    box-shadow: none;
                     color: white;
                     background-color: #eaa77f;
                     cursor: pointer;
                 }
                 &:active {
+                    top: 2px;
+                    box-shadow: none;
                     background-color: orange;
                 }
             `}
